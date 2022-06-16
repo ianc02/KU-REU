8517619af6a4986b3bfe592effce7f7dea41ab04
Ian Campbell <campbellni@hendrix.edu>
2022-06-16 11:29:26 -0500

    update

--- rdf/main.py
+++ rdf/main.py
@@ -1,6 +1,6 @@
 import math
 
-
+import numpy as np
 import time
 
 
@@ -38,21 +38,30 @@ def main():
         if i == 7:
             bins = int(file_list[i].split()[-1])
 
-    f2 = open("traj.xyz", 'r')
+    f2 = open("tmao_traj.xyz", 'r')
     f2_list = f2.readlines()
 
     d = {}
-    c = 0
+    c =0
+    g = int(f2_list[1].split()[-1])
+    print(c)
     for i in f2_list:
         i = i.split()
         if len(i) == 3:
-            c = int(i[-1]) - 1000000
-            if c > 1000:
+            c = int(i[-1]) - g
+            if c > 1000000:
                 break
             d[c] = []
 
         elif len(i) == 4:
             d[c].append(i)
+
+    atom_to_mol = {}
+    f3 = open("data_input", 'r')
+    f3_list = f3.readlines()
+    for i in f3_list:
+        i = i.split()
+        atom_to_mol[int(i[0])-1] = int(i[1])
 
     vol = l1 * l2 * l3
     dr = l1 / (2 * bins)
@@ -73,7 +82,6 @@ def main():
     for a in range(0, 1050, 50): #maybe an issue where steps == 1000 in input file but only iterate 20 times?
 
         step = d[a]
-
         for i in range(atoms - 1):
 
             if step[i][0] == r1:
@@ -81,36 +89,38 @@ def main():
                     na1 += 1
                 for j in range(i + 1, atoms):
                     if step[j][0] == r2:
-                        if a<1 and i <1:
-                            na2 +=1
-                        # if molecule1 != molecule
-                        # Not necessary rn because one molecule only has one O
+                        if atom_to_mol[i] != atom_to_mol[j]:
+                            if a<1 and i <1:
+                                na2 +=1
+
+                            # if molecule1 != molecule
+                            # Not necessary rn because one molecule only has one O
 
 
-                        r1x = float(step[i][1])
-                        r1y = float(step[i][2])
-                        r1z = float(step[i][3])
-                        r2x = float(step[j][1])
-                        r2y = float(step[j][2])
-                        r2z = float(step[j][3])
+                            r1x = float(step[i][1])
+                            r1y = float(step[i][2])
+                            r1z = float(step[i][3])
+                            r2x = float(step[j][1])
+                            r2y = float(step[j][2])
+                            r2z = float(step[j][3])
 
-                        r1x = r1x - l1 * round((r1x - r2x) / l1)
-                        r1y = r1y - l2 * round((r1y - r2y) / l2)
-                        r1z = r1z - l3 * round((r1z - r2z) / l3)
-
-
-                        r = math.sqrt(((r1x - r2x) ** 2) + ((r1y - r2y) ** 2) + ((r1z - r2z) ** 2))
-
-                        # r = math.sqrt((((float(step[j][1]) - float(step[i][1])) ** 2) + ((float(step[j][2]) -
-                        # float(step[i][2])) ** 2) + ( (float(step[j][3]) - float(step[i][3])) ** 2)))
-
-                        if r <= rmax:
+                            r1x = r1x - l1 * round((r1x - r2x) / l1)
+                            r1y = r1y - l2 * round((r1y - r2y) / l2)
+                            r1z = r1z - l3 * round((r1z - r2z) / l3)
 
 
-                            binn = (int)(r // dr)
+                            r = math.sqrt(((r1x - r2x) ** 2) + ((r1y - r2y) ** 2) + ((r1z - r2z) ** 2))
 
-                            hist[binn] += 2
-                            total += binn
+                            # r = math.sqrt((((float(step[j][1]) - float(step[i][1])) ** 2) + ((float(step[j][2]) -
+                            # float(step[i][2])) ** 2) + ( (float(step[j][3]) - float(step[i][3])) ** 2)))
+
+                            if r <= rmax:
+
+
+                                binn = (int)(r // dr)
+
+                                hist[binn] += 2
+                                total += binn
     na2 += 1
     total = total / atoms
     # print(total * vn)
@@ -136,7 +146,8 @@ def main():
     # print("***")
     f = open("py_OO.dat", "w")
     for i in range(bins):
-        # print("*" * int(1 + hist[i] / 10))
+
+        #print("*" * int(round(g[i]*10)))
         f.write("%s     %s \n" % (i * dr, g[i]))
     f.close()
 
